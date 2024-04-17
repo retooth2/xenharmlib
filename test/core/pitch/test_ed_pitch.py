fe76681bb5fb97b9c16accf6df346c932d6cae0f
Fabian Vallon <fabian.vallon@gmail.com>
2024-04-17 09:59:55 +0200

    fix(test): Improved tests for EDPitch
    * added docs
    * removed unused imports
    * added parametrization to test_pitch_periodic_indices
    * exchanged inline tuning creation to global tuning usage
    * fixed parametrization in test to make sure tuning context
      is checked by object-identity

--- test/core/pitch/test_ed_pitch.py
+++ test/core/pitch/test_ed_pitch.py
@@ -3,64 +3,72 @@ from xenharmlib.core.frequencies import Frequency
 from xenharmlib.core.tunings import EDTuning
 from xenharmlib.core.pitch import EDPitch
 from xenharmlib.exc import IncompatibleTunings
-from xenharmlib.exc import InvalidPitchIndex
-from xenharmlib.exc import InvalidPitchClassIndex
-from xenharmlib.exc import InvalidBaseIntervalIndex
 from xenharmlib.exc import InvalidGenerator
 
 edo12 = EDTuning(12, Frequency(2))
+edo17 = EDTuning(17, Frequency(2))
 edo24 = EDTuning(24, Frequency(2))
 edo31 = EDTuning(31, Frequency(2))
 ed13_3 = EDTuning(13, Frequency(3))
+ed17_3 = EDTuning(17, Frequency(3))
+
+@pytest.mark.parametrize(
+    'tuning, pitch_index, exp_pc, exp_bi',
+    [
+        (edo31, 12, 12, 0),
+        (edo31, 32, 1, 1),
+        (edo31, -1, 30, -1),
+        (edo12, -13, 11, -2),
+        (edo24, 48, 0, 2),
+        (edo24, -49, 23, -3),
+    ]
+)
+def test_pitch_periodic_indices(tuning, pitch_index, exp_pc, exp_bi):
+    """
+    Test if pitch class index and base interval index
+    are calculated correctly from pitch index
+    """
+
+    pitch = EDPitch(tuning, pitch_index)
+    assert pitch.pc_index == exp_pc
+    assert pitch.bi_index == exp_bi
 
 
-def test_pitch_periodic_indices():
 
-    pitch = EDPitch(edo31, 12)
-    assert pitch.pc_index == 12
-    assert pitch.bi_index == 0
+@pytest.mark.parametrize(
+    'tuning, pitch_index, diff, exp_result',
+    [
+        (edo31, 12, 2, 74),
+        (edo31, 12, -2, -50),
+        (edo12, 12, -1, 0),
+        (ed13_3, 12, 1, 25),
+    ]
+)
+def test_transpose_bi_index(tuning, pitch_index, diff, exp_result):
+    """
+    Test if method transpose_bi_index works correctly
+    """
 
-    pitch = EDPitch(edo31, 32)
-    assert pitch.pc_index == 1
-    assert pitch.bi_index == 1
-
-    pitch = EDPitch(edo31, -1)
-    assert pitch.pc_index == 30
-    assert pitch.bi_index == -1
-
-
-def test_transpose_bi_index():
-
-    pitch = EDPitch(edo31, 41)
-    assert pitch.pc_index == 10
-    assert pitch.bi_index == 1
-
-    transposed = pitch.transpose_bi_index(2)
-    assert transposed.pitch_index == 103
+    pitch = EDPitch(tuning, pitch_index)
+    transposed = pitch.transpose_bi_index(diff)
+    assert transposed.pitch_index == exp_result
 
 
 @pytest.mark.parametrize(
     'tuning, pitch_index, diff, new_index',
     [
-        (
-            EDTuning(12, Frequency(2)),
-            7, 10, 17
-        ),
-        (
-            EDTuning(17, Frequency(2)),
-            7, -3, 4
-        ),
-        (
-            EDTuning(17, Frequency(3)),
-            19, 3, 22
-        ),
-        (
-            EDTuning(12, Frequency(2)),
-            -7, 10, 3
-        ),
+        (edo12, 7, 10, 17),
+        (edo17, 7, -3, 4),
+        (ed17_3, 19, 3, 22),
+        (edo12, -7, 10, 3),
     ]
 )
 def test_transpose_int(tuning, pitch_index, diff, new_index):
+    """
+    Test if transpose method with an integer parameter
+    works correctly
+    """
+
     pitch = tuning.pitch(pitch_index)
     transposed = pitch.transpose(diff)
     assert type(pitch) is EDPitch
@@ -71,25 +79,18 @@ def test_transpose_int(tuning, pitch_index, diff, new_index):
 @pytest.mark.parametrize(
     'tuning, pitch_index, diff, new_index',
     [
-        (
-            EDTuning(12, Frequency(2)),
-            7, 10, 17
-        ),
-        (
-            EDTuning(17, Frequency(2)),
-            7, -3, 4
-        ),
-        (
-            EDTuning(17, Frequency(3)),
-            19, 3, 22
-        ),
-        (
-            EDTuning(12, Frequency(2)),
-            -9, 13, 4
-        ),
+        (edo12, 7, 10, 17),
+        (edo17, 7, -3, 4),
+        (ed17_3, 19, 3, 22),
+        (edo12, -9, 13, 4),
     ]
 )
 def test_transpose_interval(tuning, pitch_index, diff, new_index):
+    """
+    Test if transpose method with a pitch interval parameter
+    works correctly
+    """
+
     pitch_a = tuning.pitch(pitch_index)
     pitch_b = tuning.pitch(pitch_index + diff)
     interval = pitch_a.interval(pitch_b)
@@ -115,13 +116,21 @@ def test_transpose_interval(tuning, pitch_index, diff, new_index):
     ]
 )
 def test_get_generator_index(pitch, gen_pitch, gen_index):
+    """
+    Test if get_generator_index works correctly
+    """
+
     result = pitch.get_generator_index(gen_pitch)
     assert result == gen_index
 
 
 def test_get_generator_index_invalid_generator():
-    pitch = EDPitch(edo12, 8)
+    """
+    Test if get_generator_index raises InvalidGenerator if
+    given parameter is not a generator pitch
+    """
 
+    pitch = EDPitch(edo12, 8)
     with pytest.raises(InvalidGenerator):
         pitch.get_generator_index(
             EDPitch(edo12, 4)
@@ -141,6 +150,10 @@ def test_get_generator_index_invalid_generator():
     ]
 )
 def test_get_bi_normalized(pitch):
+    """
+    Test if get_bi_normalized method works correctly
+    """
+
     expected = EDPitch(pitch.tuning, pitch.pc_index)
     assert pitch.get_bi_normalized() == expected
 
@@ -156,6 +169,10 @@ def test_get_bi_normalized(pitch):
     ]
 )
 def test_retune(pitch_a, pitch_b):
+    """
+    Test if retune method works correctly
+    """
+
     assert pitch_a.retune(pitch_b.tuning) == pitch_b
     assert pitch_b.retune(pitch_a.tuning) == pitch_a
 
@@ -174,8 +191,16 @@ def test_retune(pitch_a, pitch_b):
     ]
 )
 def test_lt_gt(pitch_a, pitch_b):
+    """
+    Test if pitches can be compared with lesser-than
+    and greater-than relations and if lesser-than and
+    greater-than also implies inequality
+    """
+
     assert pitch_a < pitch_b
     assert pitch_b > pitch_a
+    assert pitch_a != pitch_b
+    assert pitch_b != pitch_a
 
 
 @pytest.mark.parametrize(
@@ -189,6 +214,10 @@ def test_lt_gt(pitch_a, pitch_b):
     ]
 )
 def test_eq(pitch_a, pitch_b):
+    """
+    Test if two pitches are correctly recognized as
+    equal if they have the same frequency
+    """
     assert pitch_a == pitch_b
 
 
@@ -198,9 +227,15 @@ def test_eq(pitch_a, pitch_b):
         (EDPitch(edo12, 3), EDPitch(edo12, 3)),
         (EDPitch(edo31, 9), EDPitch(edo31, 40)),
         (EDPitch(edo24, 3), EDPitch(edo24, 51)),
+        (EDPitch(edo12, 2), EDPitch(edo24, 52)),
     ]
 )
 def test_is_equivalent(pitch_a, pitch_b):
+    """
+    Test if two pitches are correctly recognized as
+    equivalent if their frequency is the same under
+    base interval normalization
+    """
     assert pitch_a.is_equivalent(pitch_b)
 
 
@@ -213,17 +248,28 @@ def test_is_equivalent(pitch_a, pitch_b):
     ]
 )
 def test_is_not_equivalent(pitch_a, pitch_b):
+    """
+    Test if two pitches are correctly recognized as
+    not equivalent if their frequency is not the same under
+    base interval normalization
+    """
     assert not pitch_a.is_equivalent(pitch_b)
 
 
 @pytest.mark.parametrize(
     'pitch_b',
     [
+        EDPitch(edo12, 3),
         EDPitch(edo31, 0),
         EDPitch(ed13_3, 7),
     ]
 )
-def test_incompatible_tunings(pitch_b):
+def test_get_generator_index_incompatible_tunings(pitch_b):
+    """
+    Test if IncompatibleTunings is raised while calling
+    get_generator_index when generator pitch is from a different
+    tuning context
+    """
 
     edo12_2 = EDTuning(12, Frequency(2))
     pitch_a = EDPitch(edo12_2, 0)
@@ -242,6 +288,10 @@ def test_incompatible_tunings(pitch_b):
     ]
 )
 def test_arithmetic(index_a, index_b):
+    """
+    Test if pitch addition/substraction and scalar
+    multiplication works correctly
+    """
 
     sum_result = index_a + index_b
     diff_result = index_a - index_b
@@ -277,6 +327,11 @@ def test_arithmetic(index_a, index_b):
     ]
 )
 def test_arithmetic_incompatible_tunings(index_a, index_b):
+    """
+    Test if IncompatibleTunings is raised when addition
+    and substraction is done with pitches from different
+    tuning contexts
+    """
 
     edo12_2 = EDTuning(12, Frequency(2))
     pitch_a = EDPitch(edo12, index_a)
